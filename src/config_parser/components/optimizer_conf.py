aa9420ee88695f431209585aa7573128b77dad93
rdarbinyan <darbinyanraf@gmail.com>
2021-01-28 10:47:44 +0400

    sgd optimizer option

--- src/config_parser/components/optimizer_conf.py
+++ src/config_parser/components/optimizer_conf.py
@@ -23,8 +23,20 @@ class AdamConf(OptimizerConf):
     def get_optimizer(self, model_params) -> torch.optim.Optimizer:
         return torch.optim.Adam(params=model_params, **asdict_filtered(self))
 
+@dataclass(frozen=True)
+class SgdConf(OptimizerConf):
+    lr: float
+    momentum: float
+    weight_decay: float
+    nesterov: bool
 
-valid_names = {"adam": AdamConf}
+    def get_optimizer(self, model_params) -> torch.optim.Optimizer:
+        return torch.optim.SGD(params=model_params, **asdict_filtered(self))
+
+valid_names = {
+    "adam": AdamConf,
+    "sgd": SgdConf
+}
 
 
 def get_config_obj(cfg_subgroup: DictConfig) -> OptimizerConf:
